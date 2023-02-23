bcb156a2b8f310f8941eb995017b3e7ee5392a6e
Dragesia <brkgzlsy@gmail.com>
2023-02-23 19:12:45 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,2 @@
+import Node from "./node.js";
+import Tree from "./tree.js";
---
+++ tree.js
@@ -0,0 +1,12 @@
+import Node from "./node.js";
+
+export default class Tree {
+    constructor(arr = []) {
+        this.root = buildTree(arr);
+    }
+
+    buildTree(arr) {
+        
+    }
+
+}
