5d77e31bd6e1c5338bdeb067084bd67e736d94bf
Dragesia <brkgzlsy@gmail.com>
2023-02-24 19:16:17 +0300

    add insert and insertrec method

--- tree.js
+++ tree.js
@@ -18,5 +18,55 @@ export default class Tree {
 
         return node;
     }
+    
+    insert(value) {
+        this.root = this.insertRec(value ,this.root);
+    }
+    insertRec(value, root) {
+        if (root == null) {
+            root = new Node(value);
+            return root;
+        } 
+
+        if (value < root.data) {
+            root.left = this.insertRec(value, root.left);
+        } else if (value > root.data) {
+            root.right = this.insertRec(value, root.right);
+        }
+
+        return root;
+    }
+    delete() {}
+    find() {}
+    levelOrder() {}
+    inorder() {}
+    preorder() {}
+    postorder() {}
+    height() {}
+    depth() {}
+    isBalanced() {}
+    rebalance() {}
 
 }
+
+
+
+
+
+
+/* testing */
+
+const prettyPrint = (node, prefix = '', isLeft = true) => {
+    if (node.right !== null) {
+      prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+    }
+    console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
+    if (node.left !== null) {
+      prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+    }
+  }
+
+let arr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+let newTree = new Tree(arr);
+
+prettyPrint(newTree.root);
